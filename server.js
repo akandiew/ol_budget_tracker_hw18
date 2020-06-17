a8b5a1f72d46bc9b31e22ef0b9488ef3cd3e73ac
akandiew <andrewkandiew@Andrews-MBP.home>
2020-06-17 16:28:55 -0400

    fix PORT

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// add dynamic PORT
+const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 
